4ab5e9765f0609f92ef989d349dead7d5e2e4af2
vazzori <78256402+vazzori@users.noreply.github.com>
2023-10-26 18:31:18 +0500

    upd

--- index.php
+++ index.php
@@ -10,17 +10,23 @@ require('connect.php');
     <title>Привет, мир!</title>
 </head>
 <body style="background: #280045; color: whitesmoke;">
-
+<div class="alert alert-success alert-dismissible fade show d-none" role="alert" id="success-alert" style="position: fixed; bottom: 20px; right: 20px; width: 500px">
+    Данные успешно записаны в базу данных.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
 <div class="container" style="">
     <div class="d-flex align-items-center justify-content-center" style="height:50vh;">
-        <form class="form" >
+        <form class="form" id="myForm">
             <div class="row">
                 <div class="col">
                     <input class="form-control" type="text" id="name" value="" placeholder="Введите строку"/>
                 </div>
                 <div class="col">
-                    <input class="btn btn-outline-light" type="button" value="Отправить" id="btn_submit" />
+                    <input class="btn btn-outline-light" type="submit" value="Отправить" id="btn_submit" />
                 </div>
+            </div>
         </form>
     </div>
 </div>
@@ -34,7 +40,7 @@ require('connect.php');
             <th scope="col">Дата</th>
         </tr>
         </thead>
-        <tbody>
+        <tbody id="resultTable">
         <?php
         $sth = $pdo->prepare("SELECT * FROM `results` ORDER BY `time`");
         $sth->execute();
@@ -56,16 +62,24 @@ require('connect.php');
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 <script type="text/javascript">
     $(document).ready(function(){
-        $('#btn_submit').click(function(){
+        $('#myForm').submit(function(e) {
+            e.preventDefault();
             var user_name = $('#name').val();
             $.ajax({
                 url: "script.php",
                 type: "post",
                 data: {
-                    "name":    user_name,
+                    "name": user_name,
+                },
+                success: function(data) {
+                    $('#name').val('');
+                    $('#success-alert').removeClass('d-none');
+                    setTimeout(function() {
+                        $('#success-alert').addClass('d-none');
+                    }, 3000);
                 }
             });
-            $('#name').val('');
+
         });
     });
 </script>
--- script.php
+++ script.php
@@ -1,12 +1,13 @@
 <?php
 require('connect.php');
 
+$bracketSet = ['(' => ')', '[' => ']', '{' => '}', '<' => '>'];
+$name = $_POST['name'];
+isValid($name, $bracketSet);
 
-    $name = $_POST['name'];
-    isValid($name);
-
-function isValid(string $s): bool {
-    $sClear = ClearString($s);
+function isValid(string $s, $brackets)
+{
+    $sClear = ClearString($s, $brackets);
     $sLength = strlen($sClear);
 
     if ($sLength % 2 !== 0 || $sClear == '') {
@@ -14,30 +15,30 @@ function isValid(string $s): bool {
         return false;
     };
 
-    $bracketSet = ['(' => ')', '[' => ']', '{' => '}', '<' => '>'];
     $bracketStack = [];
 
     for ($i = 0; $i < $sLength; $i++) {
-        if (array_key_exists($sClear[$i], $bracketSet)) {
-            $bracketStack[] = $bracketSet[$sClear[$i]];
+        if (array_key_exists($sClear[$i], $brackets)) {
+            $bracketStack[] = $brackets[$sClear[$i]];
         } elseif (array_pop($bracketStack) !== $sClear[$i]) {
             WriteBD(0, $s);
             return false;
         }
     }
-    if(count($bracketStack) === 0){
+    if (count($bracketStack) === 0) {
         WriteBD(1, $s);
-    }else{
+    } else {
         WriteBD(0, $s);
         return false;
     }
 }
 
-function ClearString($s): string{
+function ClearString($s, $brackets): string
+{
     $clearArr = [];
     $stringArr  = str_split(str_replace(' ', '', $s));
     foreach ($stringArr as $char){
-        if($char == '(' || $char == '[' || $char == '{' || $char == '<' || $char == ')' || $char == ']' || $char == '}' || $char == '>')
+        if(array_key_exists($char, $brackets) || in_array($char, $brackets))
         {
             $clearArr[] = $char;
         }
@@ -53,3 +54,4 @@ function WriteBD($result, $initialLine){
     $sth = $pdo->prepare("INSERT INTO `results` SET `result` = :result, `time` = :time, `initialLine` = :initialLine ");
     $sth->execute(array('result' => $result, 'time' => date("Y-m-d H:i:s"), 'initialLine' => $initialLine));
 }
+
